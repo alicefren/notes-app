f1e687e6f3cda94af665c573ca5a7e3ad3beb24a
alicefren <78852401+alicefren@users.noreply.github.com>
2021-04-01 12:57:27 +0100

    Removed Dates
    Notes about dates in JS removed from notes-app

--- notes-app.js
+++ notes-app.js
@@ -37,34 +37,3 @@ window.addEventListener('storage', function (e) {
     renderNotes(notes, filters)
     }   
 })
-
-/* DATES
-JavaScript
-
-const firstDate = new Date ('March 16 2021 12:37:00')
-const secondDate = new Date ('April 18 1992 08:30:33')
-const firstDateTimestamp = firstDate.getTime()
-const secondDateTimestamp = secondDate.getTime()
-
-if (firstDateTimestamp < secondDateTimestamp) {
-    console.log(firstDate.toString())
-} else if (secondDate < firstDate) {
-    console.log(secondDate.toString())
-}
-
-
-Moment
-
-const now = moment()
-now.add(1, 'year').subtract(20, 'days')
-console.log(now.format("MMMM Do, YYYY"))
-console.log(now.fromNow())
-const nowTimestamp = now.valueOf()
-console.log(nowTimestamp)
-
-const birthday = moment()
-birthday.year(1992).month(3).day(18)
-
-console.log(birthday,format("MMM D YYYY"))*/
-
-
